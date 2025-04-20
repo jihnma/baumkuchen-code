79bd22a3caa711e6459c737274aa4b0d5525c7c9
jihnma <178634468+jihnma@users.noreply.github.com>
2025-04-20 22:18:45 +0900

    revert

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./app.tsx";
 
 const root = document.getElementById("root");
-
 if (!root) throw new Error("Root element with ID 'root' not found in the DOM");
 
 createRoot(root).render(
