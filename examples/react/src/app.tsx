8c82bcca7553c3910618999023c5a02ed25fec21
jihnma <178634468+jihnma@users.noreply.github.com>
2025-04-20 22:37:33 +0900

    update tailwindcss

--- examples/react/src/app.tsx
+++ examples/react/src/app.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <>Hello, World!</>;
+  return (
+    <h1 className="text-6xl font-extrabold text-white [text-shadow:_4px_4px_0_#ef4444,_8px_8px_0_#3b82f6] hover:[text-shadow:_4px_4px_0_#3b82f6,_8px_8px_0_#ef4444] transition-all duration-300">
+      Hello world!
+    </h1>
+  );
 }
