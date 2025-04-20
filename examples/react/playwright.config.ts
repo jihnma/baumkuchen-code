61c868af26d24e5afc5dfb344753a2f069a31775
jihnma <178634468+jihnma@users.noreply.github.com>
2025-04-20 20:18:09 +0900

    e2e

--- examples/react/playwright.config.ts
+++ examples/react/playwright.config.ts
@@ -5,6 +5,14 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./e2e",
+  timeout: 30 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000,
+  },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
